f661b4d282dee9051bb70d785c7d9b54e7ff18da
SilverSurver <yedibela.enes@gmail.com>
2024-11-28 20:22:21 +0100

    new

--- programm.py
+++ programm.py
@@ -1,133 +1,149 @@
 import os
 import shutil
+import signal
 import subprocess
 
+def tag_arr_to_str(tags):
+    ret = ''
+    for tag in tags:
+        ret+=(str(tag)+' ')
+    return ret.strip()
 
-def split_in_ors(command):
-    return [i.strip() for i in command.strip().split('|')]
+def handle_put(dest_bwz_source,tx):
+    move_counter = 0
+    source = 'download_folder/'
+    with open(tx,'a') as file:
 
-def split_in_ands(ors):
-    return [[i.strip() for i in k.strip().split(' ')] for k in ors]
+        for filename in os.listdir(source):
+            move_counter+=1
+            full_path = os.path.join(source, filename)
+            process = subprocess.Popen(f'gio open {full_path}', shell=True)
+            tags = input("tags : ")
+            tag_arr = tags.split(' ')
+            wr = tag_arr_to_str(fill_tags(tag_arr))
 
 
-def contains_and(or_block, two):
-    # or block: [and_one, and_two, and_three]
-    for i in or_block:
-        if not two.__contains__(i):
-            return False
-    return True
+            file.write(f"{filename} {wr}\n")
 
-def add_or(tags,one,two,full_tags):
+            process.kill()
+            process.wait()
+
+            subprocess.run("pkill xviewer", shell=True, check=True)
+        file.close()
+
+        if move_counter > 0:
+            subprocess.run(f'mv {source}* {dest_bwz_source}',shell=True,check=True)
+
+
+def filter_fits(filter_arr, tag_arr):
+    for or_block in filter_arr:
+        contains_all = False
+        for and_el in or_block:
+            if tag_arr.__contains__(and_el):
+                contains_all = True
+            else:
+                contains_all = False
+                break
+        if contains_all:
+            return True
+    return False
+
+
+
+def handle_filter(source,dest,tx):
+    move_counter = 0
+    filter = input('filter: ').strip()
+    ors = [i.strip() for i in filter.split('|')]
+    ands = [[i.strip() for i in or_block.strip().split(' ')] for or_block in ors]
+
+    with open(tx,'r') as reading:
+        for line in reading:
+            image,tags = str_to_tag_arr(line)
+            if filter_fits(ands,tags):
+                shutil.move(os.path.join(source,image),dest)
+                move_counter += 1
+
+        reading.close()
+
+    subprocess.run(f'gio open {dest}',shell=True,check=True)
+    return move_counter > 0
+
+
+def add_or(one,two,tags):
     put = False
-    for i in one:
-        if tags.__contains__(i) or full_tags.__contains__(i):
+    for tag in one:
+        if tags.__contains__(tag):
             put = True
             break
-
     if put:
-        for i in two:
-            if not (full_tags.__contains__(i) or tags.__contains__(i)):
-                full_tags.append(i)
+        for tag in two:
+            if not tags.__contains__(tag):
+                tags.append(tag)
+    return tags
 
-
-def add_and(tags,one,two,full_tags):
+def add_and(one,two,tags):
     put = False
-    for i in one:
-        if full_tags.__contains__(i):
+    for tag in one:
+        if tags.__contains__(tag):
             put = True
         else:
             put = False
             break
 
     if put:
-        for i in two:
-            if not full_tags.__contains__(i):
-                full_tags.append(i)
+        for tag in two:
+            if not tags.__contains__(tag):
+                tags.append(tag)
+    return tags
 
+def fill_tags(tags):
+    tags = add_or(['hund'], ['doggo'], tags)
+    tags = add_or(['doggo','kitty'],['cute','sues'],tags)
+    tags = add_and(['doggo','cute'],['super'],tags)
+    tags = add_and(['kitty', 'cute'], ['super'], tags)
+    tags = add_and(['k', 'j',], ['noice'], tags)
 
-def add(tags,full_tags):
-    # tuhe bei or immer den vollen Namen überall rein, also auch bei weitereen:
-    # add_or(tags, ['d'], ['doggo','immer_knuffig'], full_tags)
-    add_or(tags, ['d'], ['doggo','knuffig'], full_tags)
-    add_or(tags, ['kn','knuffig'], ['knuffig'], full_tags)
-    add_or(tags, ['k'], ['kitty'], full_tags)
-    add_or(tags, ['doggo', 'kitty'], ['cute'], full_tags)
+    return tags
 
+def str_to_tag_arr(im_ta_str):
+    tmp = im_ta_str.strip().split(' ')
+    im = tmp[0]
+    temp = tmp[1:]
+    return im.strip() ,[i.strip() for i in temp]
 
-    # bei dem hier müssen alle volle Namen enthalten
-    add_and(tags, ['doggo', 'cute','knuffig'], ['sugar'], full_tags)
+def handle_update(tx,tmp):
+    with open(tx,'r') as reading , open(tmp,'w') as writing:
+        for line in reading:
+            im,tags = str_to_tag_arr(line)
+            tags = fill_tags(tags)
+            writing.write(f"{im} {tag_arr_to_str(tags)}\n")
+        reading.close()
+        writing.close()
 
-    ret = ''
-    for i in full_tags:
-            ret+=(i +' ')
-    return ret
+    with open(tmp, 'r') as reading, open(tx, 'w') as writing:
+        for line in reading:
+            writing.write(line)
+
+        reading.close()
+        writing.close()
+
 
 
 if __name__ == '__main__':
-    directory = "cute/"
-    source = "filter/"
-    obj = []
-    with open('file_name.txt','r') as file:
-        for line in file:
-            tmp = line.split(' ')
-            one = tmp[0].strip()
-            two = [i.strip() for i in tmp[1:]]
-            obj.append((one,two))
-        file.close()
+    source = 'cute/'
+    dest = 'filter/'
+    tx = 'file_name.txt'
+    tmp_file = 'temp.txt'
+    cmd = input('cmd: ')
+    while(cmd != 'q'):
+        if cmd == 'p':
+            handle_put(source,tx)
+        elif cmd == 'f':
+            if handle_filter(source,dest,tx):
+                cmd = input('cmd: ')
+                subprocess.run(f'mv {dest}* {source}',shell=True, check=True)
+                continue
+        elif cmd == 'u':
+            handle_update(tx,tmp_file)
 
-
-    print(obj)
-
-    cmd = input("command: ")
-
-
-    while cmd != 'q':
-        if cmd == 'f':
-            filter = input('filter: ')
-            ors = split_in_ors(filter)
-            ands = split_in_ands(ors)
-
-            for one,two in obj:
-                for or_block in ands:
-                    if contains_and(or_block,two):
-                        # move
-                        tmp = os.path.join(directory,one)
-                        shutil.move(tmp,source)
-                        break
-
-            cmd = input("command: ")
-
-            command  = "mv " + source +'* ' + directory
-            subprocess.run(command, shell=True, check=True)
-
-        elif cmd == 'a':
-            # Specify the directory
-            tag = input('tag: ')
-            tags = tag.split(' ')
-            to_add = []
-            to_remove = []
-            full_tags = []
-            tags_ = add(tags,full_tags)
-            # Iterate through all files in the directory
-            for filename in os.listdir(directory):
-                print(filename)
-                if filename[-5] == ')':
-                    to_remove.append(filename)
-                elif not any(item[0] == filename for item in obj):
-                    to_add.append(filename)
-
-
-            with open('file_name.txt','a') as writer:
-                for i in to_add:
-                    obj.append((i,full_tags))
-                    wr = i+ ' ' + tags_ + '\n'
-                    writer.write(wr)
-                writer.close()
-
-            print(obj)
-
-
-            for i in to_remove:
-                os.remove(os.path.join(directory, i))
-
-            cmd = input("command: ")
+        cmd = input('cmd: ')
